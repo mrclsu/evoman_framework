763b9b7c3a92721db58b4e404d799a4a05d4afed
AustinTD4 <101255991+AustinTD4@users.noreply.github.com>
2023-09-13 12:02:09 +0200

    Austin_Version_Specialized.py

--- Austin_Version_Specialized.py
+++ Austin_Version_Specialized.py
@@ -1,40 +1,28 @@
 #######################################################################################
-# EvoMan EA			                              		              #
-# DEMO : perceptron neural network controller evolved by Genetic Algorithm            #
+# EvoMan EA			                              		                        	  #
+# DEMO : perceptron neural network controller evolved by Genetic Algorithm        	  #
 #                                                                                     #
-# Author: Austin Dickerson       			                              #
+# Author: Austin Dickerson       			                                  		  #
 #######################################################################################
-
+#%%
 # imports framework
 import sys, os
-
 from evoman.environment import Environment
 from demo_controller import player_controller
-
 import numpy as np
 import random
+
 
 experiment_name = 'controller_specialist_demo'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
-# Update the number of neurons for this specific example
-n_hidden_neurons = 10
+env = Environment()
 
-# initializes environment for single objective mode (specialist)  with static enemy and ai player
-env = Environment(experiment_name=experiment_name,
-				  playermode="ai",
-				  player_controller=player_controller(n_hidden_neurons),
-			  	  speed="fastest",
-				  enemymode="static",
-				  level=2,
-				  visuals=False)
-print(env.get_num_sensors())
+n_hidden_neurons = 10
 num_vars = n_hidden_neurons*(env.get_num_sensors()+1) + (n_hidden_neurons+1)*5
-mutagenic_temperature = 0.3
-threshold = 0.2
 replacement = 0.5
-pop_size = 100
+pop_size = 200
 generations = 30
 
 def initialize_population(pop_size, num_vars):
@@ -43,7 +31,7 @@ def initialize_population(pop_size, num_vars):
         member = np.zeros(num_vars)
         for j in range(num_vars):
             member[j] = random.uniform(-1,1)
-        pop.append(member)
+        pop.append(member)        
     return pop
     
 def simulate(env,unit):
@@ -54,7 +42,10 @@ def test_population(pop):
     scores = []
     for unit in pop:
         scores.append(simulate(env,unit))
-    return scores, max(scores)
+    maximum = max(scores)
+    ind = scores.index(maximum)
+    scores = normalize(scores)
+    return scores, maximum, ind
 
 def normalize(scores):
     lo = min(scores)
@@ -64,7 +55,9 @@ def normalize(scores):
     return scores
 
 def combine_genes(p1, p2):
-    first_key = np.random.randint(2, len(p1))
+    temp = np.random.random()
+    first_key = np.random.random(len(p1))
+    first_key = [1 if x > temp else 0 for x in first_key]
     second_key = np.ones(len(p1))-first_key
     zygote1 = first_key*p1
     zygote2 = second_key*p2
@@ -80,14 +73,14 @@ def select_parents(pop, low, high):
     p2 = pop[second]
     return p1, p2
 
-def mutate_offspring(mutagenic_temperature, child):
+def mutate_offspring(child):
     for i in range(len(child)):
         if random.random() < mutagenic_temperature:
-            child[i] += random.uniform(-0.5,0.5)
+            child[i] += random.uniform(-mutation_intensity, mutation_intensity)
             if child[i] < -1:
-                child[i] = -1
+                child[i] = random.uniform(-1,1)
             if child[i] > 1:
-                child[i] = 1
+                child[i] = random.uniform(-1,1)
     return child
 
 def reproduce(pop, fit_scores):
@@ -99,16 +92,56 @@ def reproduce(pop, fit_scores):
     for i in range(replaced):
         p1, p2 = select_parents(pop, replaced, len(pop))
         child = combine_genes(p1, p2)
-        pop[i] = mutate_offspring(mutagenic_temperature, child)
+        pop[i] = mutate_offspring(child)
     return pop
 
+def reseed_event(pop, fit_scores):
+    print("RESEED")
+    indices = sorted(range(len(fit_scores)), key=lambda x: fit_scores[x], reverse=True)[:5]
+    for count, unit in enumerate(pop):
+        if count not in indices:
+            parent = random.randint(0,4)
+            geno_fidelity = random.random()
+            for i in range(len(unit)):
+                if random.random() > geno_fidelity:
+                    unit[i] = random.uniform(-1,1)
+                else:
+                    unit[i] = pop[indices[parent]][i]
+    return pop
 
-performance = []  
-pop = initialize_population(pop_size, num_vars)
-for i in range(generations):
-    fit_scores, maxi = test_population(pop)
-    performance.append(maxi)
-    pop = reproduce(pop, fit_scores)
-print(performance)
+def test_mutation(mutagenic_temperature, mutation_intensity):
+    globals() ['mutagenic_temperature'] = mutagenic_temperature
+    globals() ['mutation_intensity'] = mutation_intensity
+    performance = [0]  
+    pop = initialize_population(pop_size, num_vars)
+    best = 0
+    reseed_timer = 0
+    for i in range(generations):
+        fit_scores, maxi, ind = test_population(pop)
+        performance.append(maxi)
+        if maxi >= max(performance):
+            best = pop[ind]
+        if (len(performance) > 5):
+            if performance[-5] == performance[-1] and reseed_timer >= 5:
+                print(f"Generation {i+1}:")
+                pop = reseed_event(pop, fit_scores)
+            else:
+                reseed_timer += 1
+                pop = reproduce(pop, fit_scores)
+    print(performance)
+    print(max(performance))
+    return best
 
-
+bests = []
+for i in range(1,6):
+    env = Environment(experiment_name=experiment_name,
+				  playermode="ai",
+                  enemies=[1],
+				  player_controller=player_controller(n_hidden_neurons),
+			  	  speed="fastest",
+				  enemymode="static",
+				  level=2,
+				  visuals=False)
+    best = test_mutation(0.5, 0.1*i)
+    bests.append(best)
+np.savetxt("Reseed_Boss_1.csv", bests, delimiter=",")
